15babca207ced25a978d5210afa0359640d1fef1
John <johndawson.inbox@gmail.com>
2023-01-18 13:37:00 -0700

    added a fabicon and changed some CSS

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-alert('working');
-const action = document.getElementById("slimer");
+
+const action = document.getElementById("slimerPic");
+const unAction = document.getElementById("appear");
+function unMagic(){
+    action.style.visibility ="visible";
+}
 function magic(){
     action.style.visibility = "hidden";
 }
 action.addEventListener("click", magic);
+unAction.addEventListener("click", unMagic);
+
